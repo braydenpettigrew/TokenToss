e1eba4d04003670dfd5ec5fc0441b81416b1b609
Brayden Pettigrew <brayden@thepettigrews.org>
2025-04-12 11:47:58 -0400

    Complete landing page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Button from "@/components/Button";
 import PageContainer from "@/components/PageContainer";
+import { useTheme } from "@/context/ThemeContext";
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 
 export default function Home() {
+  const theme = useTheme();
+  const router = useRouter();
+
   return (
     <PageContainer>
       <Container>
@@ -9,8 +16,28 @@ export default function Home() {
           Welcome to <HighlightedText>TokenToss</HighlightedText>
         </Heading>
         <Subheading>
-          Your one-stop solution for losing all of your money
+          The one-stop solution for losing all of your money and becoming
+          financially unstable.
         </Subheading>
+        <HorizontalLine />
+        <Subheading>
+          The best part? <HighlightedText>No middlemen.</HighlightedText>
+        </Subheading>
+        <Subheading>
+          Gambling problem? <HighlightedText>No problem.</HighlightedText>
+        </Subheading>
+        <Subheading>
+          Not convinced? <HighlightedText>Grab a beer and come back.</HighlightedText>
+        </Subheading>
+        <Button
+        onClick={() => {router.push("/my-bets")}}
+          backgroundColor={theme.accent}
+          textColor={theme.background}
+          marginTop="32px"
+        >
+          Sounds Great!
+          <ArrowRight />
+        </Button>
       </Container>
     </PageContainer>
   );
@@ -32,10 +59,17 @@ const Heading = styled.h1`
 
 const Subheading = styled.h2`
   font-size: ${({ theme }) => theme.fontSize.subheading};
-  color: ${({ theme }) => theme.primaryLight};
+  color: ${({ theme }) => theme.accent};
 `;
 
 const HighlightedText = styled.span`
   color: ${({ theme }) => theme.primaryLight};
   font-weight: bold;
+`;
+
+const HorizontalLine = styled.div`
+  width: 30%;
+  height: 1px;
+  background-color: ${({ theme }) => theme.shadow};
+  margin: 16px;
 `;
